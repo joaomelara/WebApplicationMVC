e341218842ec9bf4778ff15dc7d9c794ae083d66
joaomelara <3262.343@edu.fb.org.br>
2024-02-06 21:56:22 -0300

    feat:subi com erro mesmo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace WebApplicationMVC.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new Models.ViewModel.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
